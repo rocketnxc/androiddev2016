474aa2e33971262c7ef301376179a8b2e21dda0d
Cao Xuân Ngọc <rocketnxc@gmail.com>
2016-03-18 17:04:23 +0700

    Adding Facebook SDK to project

--- FirstBlood/app/src/main/java/vn/edu/usth/firstblood/MainActivity.java
+++ FirstBlood/app/src/main/java/vn/edu/usth/firstblood/MainActivity.java
@@ -1,12 +1,10 @@
 package vn.edu.usth.firstblood;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
