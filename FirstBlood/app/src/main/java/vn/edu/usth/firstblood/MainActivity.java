143be16bfcfe9bd749ed8e97de5888d520b60721
Cao Xuân Ngọc <rocketnxc@gmail.com>
2016-03-19 22:00:32 +0700

    Login Successfully

--- FirstBlood/app/src/main/java/vn/edu/usth/firstblood/MainActivity.java
+++ FirstBlood/app/src/main/java/vn/edu/usth/firstblood/MainActivity.java
@@ -1,23 +1,117 @@
 package vn.edu.usth.firstblood;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageButton;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    LoginButton fbbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //to initialize the SDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //call CallbackManager.Factory.create to create a callback manager to handle login responses
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
-        ImageButton fbbutton = (ImageButton)findViewById(R.id.fbbutton);
-        fbbutton.setOnClickListener(new View.OnClickListener() {
+
+
+        fbbutton = (LoginButton) findViewById(R.id.login_button);
+
+        fbbutton.setReadPermissions("public_profile email");
+
+        fbbutton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PagerActivity.class);
-                startActivity(intent);
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        printKeyHash(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    public static String printKeyHash(Activity context) {
+        PackageInfo packageInfo;
+        String key = null;
+        try {
+            //getting application package name, as defined in manifest
+            String packageName = context.getApplicationContext().getPackageName();
+
+            //Retriving package info
+            packageInfo = context.getPackageManager().getPackageInfo(packageName,
+                    PackageManager.GET_SIGNATURES);
+
+            Log.e("Package Name=", context.getApplicationContext().getPackageName());
+
+            for (Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                key = new String(Base64.encode(md.digest(), 0));
+
+                // String key = new String(Base64.encodeBytes(md.digest()));
+                Log.e("Key Hash=", key);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("Name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("No such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("Exception", e.toString());
+        }
+
+        return key;
     }
 }
