ed5ad4407f568608dd9c182ebc0ea1545e57da42
Cao Xuân Ngọc <rocketnxc@gmail.com>
2016-03-25 14:03:05 +0700

    added posting status function

--- FirstBlood/app/src/main/java/vn/edu/usth/firstblood/fragments/NewsFeedFragment.java
+++ FirstBlood/app/src/main/java/vn/edu/usth/firstblood/fragments/NewsFeedFragment.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 import vn.edu.usth.firstblood.R;
 
@@ -15,15 +19,30 @@ import vn.edu.usth.firstblood.R;
  */
 public class NewsFeedFragment extends Fragment {
     public NewsFeedFragment() {
+
     }
 
+    private ShareDialog shareDialog;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = new View(getActivity());
         view = inflater.inflate(R.layout.fragment_news_feed, container, false);
+        ImageButton imgbt;
+        imgbt = (ImageButton) view.findViewById(R.id.imageButton1);
+        imgbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareDialog = new ShareDialog(getActivity());
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
+            }
+        });
+
         return view;
     }
+
+
 }
 
 
